87d54c1fdbed92242e577faa1f680adc707a13b1
Bill Denney <wdenney@humanpredictions.com>
2022-03-14 12:27:17 -0400

    better submodule handling

--- test_nlmixr.sh
+++ test_nlmixr.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 
-# Packages must be listed in dependency order
+# Packages must be listed in dependency order and must be submodules
+# in the "packages" directory of this git repo
 packages=("nlmixr2data" "lotri" "rxode2" "nlmixr2est" "nlmixr2plot" "nlmixr2extra" "nlmixr2")
 
 base_clone_url="git@github.com:nlmixr2/"
 # for testthat
 NOT_CRAN="true"
 
+# Update everything
+git submodule update --recursive --remote
+git commit -am "submodules updated"
+
+# Remove the old results and prepare for the new
+git rm outputs/*txt
+mkdir -p outputs
+
 for current_dir in ${packages[@]}; do
   echo Starting $current_dir
-  if [ ! -d ${current_dir} ]; then
-    clone_url="${base_clone_url}${current_dir}.git"
-    git clone "${clone_url}"
-  fi
-  if [ ! -d ${current_dir} ]; then
-    echo "${clone_url}"
-    exit 1
-  fi
   (
-    cd $current_dir
-    git checkout main
-    git pull
+    cd packages/$current_dir
     # Clean up old files
     git clean -d -x -f
     # The 2>&1 redirects stderr to stdout
     # The tee copies stdout to a file and stdout
     R -e "devtools::install_local(force=TRUE)" 2>&1 | \
-	tee ../outputs/${current_dir}_install.txt
+	tee ../../outputs/${current_dir}_install.txt
     if [[ $? -ne 0 ]] ; then
       echo Error installing ${current_dir}
-      exit 1
+      break
     fi
     if [ -d tests/testthat ]; then
       # The 2>&1 redirects stderr to stdout
       # The tee copies stdout to a file and stdout
       R -e "devtools::load_all();devtools::test()" 2>&1 | \
-	tee ../outputs/${current_dir}_test.txt
+	tee ../../outputs/${current_dir}_test.txt
       if [[ $? -ne 0 ]] ; then
         echo Crash while testing ${current_dir}
         echo Try R -g gdb then r at the prompt
-        exit 1
+        break
       fi
     else
       echo "No test directory found (tests/testthat)"
@@ -47,3 +46,8 @@ for current_dir in ${packages[@]}; do
   )
   echo Completed $current_dir
 done
+
+# Push the updates:
+git add outputs/*txt
+#git commit -m "Testing update on $(date --iso-8601)"
+#git push
